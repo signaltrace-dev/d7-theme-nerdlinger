c4e4ece2c896827910d196ac4c7c76e782e802cd
Jeff Glenn <jeff@signaltrace.net>
2016-08-18 16:19:24 -0400

    Initial commit

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function nerdlinger_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function nerdlinger_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function nerdlinger_preprocess_node(&$variables) {
+}
